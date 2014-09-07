f5af8ce115a1c44d073215fa32b33386809adf9d
klaoude <klaoude@vm-0.klaoude.koding.kd.io>
2014-09-07 20:39:46 +0000

    upgrade server and bug fix of input()

--- Client.py
+++ Client.py
@@ -1,16 +1,17 @@
 import socket
 
+from tkinter import *
+
 hote = "127.0.0.1"
-port = 28960
+port = 28961
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.connect((hote, port))
 print("Conexion done on port {}".format(port))
 
-cmd = b""
-while cmd != b"fin":
-	cmd = input("> ")
-	cmd = cmd.encode()
+cmd = ""
+while cmd != "fin":
+	cmd = raw_input("> ")
 	serv.send(cmd)
 	recv = serv.recv(1024)
 	print(recv)
--- Server.py
+++ Server.py
@@ -1,7 +1,11 @@
 import socket
 
 hote = ''
-port = 28960
+port = 28961
+
+gui = False
+ju = False
+cle = False
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.bind((hote, port))
@@ -14,7 +18,16 @@ cmd = b""
 while cmd != b"fin":
 	cmd = client.recv(1024)
 	print(cmd.decode())
-	client.send(b"done !")
+	if(cmd == "stop gui" and gui == True):
+	    client.send(b"connexion of guillaume stop !")
+	    gui = False
+	elif(cmd == "stop gui" and gui == False):
+	    client.send(b"connexion of guillaume is already stop !")
+	elif(cmd == "start gui" and gui == False):
+	    client.send(b"connexion of guillaume start !")
+	    gui = True
+	else:
+	    client.send("error")
 
 print("closed")
 client.close()
